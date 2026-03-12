e16783827af113cfdadfb0b1e587d7f3b729200e
JJ950407 <tu_correo@github.com>
2026-03-11 20:25:49 -0600

    Revert "Open delivered active orders directly in history ticket"
    This reverts commit 474456d517b0c46209145c58668a998c4886e807.

--- waiter-app/app.js
+++ waiter-app/app.js
@@ -1407,7 +1407,7 @@ function renderActivePanel() {
         renderActivePanel();
       });
     } else if (order.status === "delivered") {
-      button.addEventListener("click", () => openHistoryForOrder(order.id));
+      button.addEventListener("click", () => renderPaymentPreviewTicket(order));
     } else {
       button.disabled = true;
     }
